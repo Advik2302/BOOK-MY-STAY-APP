ba90f70dcee138eab0cf323ff3f45e29a2fdefe2
Advik2302 <Advik2302@users.noreply.github.com>
2026-03-16 09:29:48 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,10 @@
+/**
+ * Book My Stay Application - Hotel Booking Management System
+ * 
+ * @author b1p22_ra2411026010336
+ */
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        // Base structure
+    }
+}
